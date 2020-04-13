4807ad31f569227b1d3c03ce581a32c6f758dd0d
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-04-13 11:40:06 -0400

    declarando variables a los id necesarios y funcion card lista

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
 
+// //1.guardamos elementos para poder trabajar( guardar data)
+const data = window.RICKANDMORTY.results;
+//data filtrada
 
+//Crear variables para los selectores 
+const select = document.getElementById("selectores");
+select.style.display = "none";
+
+const textoSelect = document.getElementById("texto-select");
+textoSelect.style.display = "none";
+
+const contenedorPersonajes = document.getElementById("printScreenCharacters");
+const filterSpecies = document.getElementById("filterSelector");
+const contentWelcome = document.getElementById("content-welcome");
+
+// FUNCION CARD Y MOSTARR 
+function cards(data){
+  data.forEach(show=>{
+ 
+  contenedorPersonajes.innerHTML += 
+     `<div class= "card">
+       <img src="${show.image}" alt="">
+       <h3>${show.name}</h3
+       <p>${show.species}</p>
+       <p>${show.status}</p>
+       <p>${show.gender}</p>
+        </div>`
+    });  
+  
+  } 
 
 
 
