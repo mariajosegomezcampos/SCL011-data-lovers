5f4f33362e55edd25fed12ebeb0416b2b6eabb75
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-04-13 11:38:01 -0400

    funciones de filtrado y ordenasr lista

--- src/data.js
+++ src/data.js
@@ -1,1 +1,36 @@
 
+
+/* Manejo de data */
+
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
+
+
+
+window.filter = {
+  
+  species: (data,condition) =>{
+    const speciesResult = data.filter(element => {
+      return element.species == condition;
+    });
+    return speciesResult;
+  }
+
+}
+
+window.sort = {
+ order: (data,sortOrder) =>{
+   if (sortOrder=="az"){
+     const sortData = data.sort((a,b)=>{
+       return a.name.localeCompare(b.name);
+     })
+     return sortData;
+   }
+   else {
+     const sortDataZ = data.sort((a,b)=>{
+       return b.name.localeCompare(a.name);
+     })
+     return sortDataZ;        
+   }
+ } 
+}
